e1a88846401778b2ca1fbc1c8484e3fdc1ddde00
Omkar Chebale <omkarchebale0@gmail.com>
2025-04-01 00:39:53 +0530

    - build  tabs section - import assets - unnecessary file clean up

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center"
-    >
+    <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-orange-500 font-bold ">Welcome !</Text>
     </View>
   );
